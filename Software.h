18c1ed73fecc409b2288ce4f9958732bf6d51535
anyaco2 <135170521+anyaco2@users.noreply.github.com>
2024-04-05 21:56:10 +0300

    Update Software.h

--- Software.h
+++ Software.h
@@ -3,13 +3,13 @@
 #include <string>
 class Software {
 private:
-	string SystemSoftware;
-	string OperatingSystem;
+	std::string SystemSoftware;
+	std::string OperatingSystem;
 protected:
 	virtual void display_header()const;
 	virtual void display_info()const;
 public:
-	Software(string SystemSoftware1, string OperatingSystem1) {
+	Software(std::string SystemSoftware1, std::string OperatingSystem1) {
 		SystemSoftware = SystemSoftware1;
 		OperatingSystem = OperatingSystem1;
 	};
@@ -17,7 +17,7 @@ public:
 	{
 	};
 	~Software();
-	virtual string Is_SystemOperating()const;
-	virtual string get_OperatingSystem()const;
+	virtual std::string Is_SystemOperating()const;
+	virtual std::string get_OperatingSystem()const;
 	virtual void display()const;
 };
